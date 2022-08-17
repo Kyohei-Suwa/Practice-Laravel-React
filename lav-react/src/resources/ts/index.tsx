804f3608bb2edac899aa3d16970c1b3d28f1e38b
Kyohei_Suwa <suwakyo@gmail.com>
2022-08-17 22:21:02 +0900

    EsLint And DevContainer Setting

--- lav-react/src/resources/ts/index.tsx
+++ lav-react/src/resources/ts/index.tsx
@@ -1,14 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-
-const App: React.FC = () => {
-    return (
-        <div>
-            <p>Hello Laravel + React!!</p>
-        </div>
-    )
-}
+const App: React.FC = () => (
+  <div>
+    <p>Hello Laravel + React!!</p>
+  </div>
+);
 
 const container = document.getElementById('app') as HTMLInputElement;
 const root = ReactDOM.createRoot(container);
